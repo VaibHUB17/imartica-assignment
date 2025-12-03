7e535e8e318a0fac662f641639bbf36bdd4adeac
Vaibhav <vaibhavshivhare1709@gmail.com>
2025-12-03 18:01:07 +0530

    changes

--- frontend/install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Installing Imarticus Learning Frontend Dependencies...
-echo.
-npm install
-echo.
-echo Installation complete!
-echo Run 'npm run dev' to start the development server.
-pause
--- frontend/start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting Imarticus Learning Frontend...
-echo.
-echo Make sure the backend server is running on http://localhost:5000
-echo.
-npm run dev
